12a61c5f87778c4f2b20d299e4b5d8cca3fedd3d
makena <roselynemakena@gmail.com>
2017-01-22 19:24:15 +0300

    added truncate to desc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,5 @@ class ApplicationController < ActionController::Base
 
     # Overwriting the sign_out redirect path method
 private
-  def after_sign_up_path_for(resource_or_scope)
-    home_path
-  end
-
-  def after_inactive_sign_up_path_for(resource)
-    	new_user_session_path
-	end
   
 end
